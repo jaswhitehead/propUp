cf7a6f582bbf18bfe9b23577da85a431a2de93c9
cjohnson1580 <cjohnson1580@gmail.com>
2020-09-11 21:27:33 -0500

    push to git

---
+++ dummydata.js
@@ -0,0 +1,109 @@
+this.state = {
+    ownerID: localStorage.getItem(LoginString.FirebaseDocumentId),
+    name: "Derrick Henry",
+    address: "264 1st Avenue",
+    province: "Nashville",
+    zipC: "37203",
+    minBid: "150",
+    pic: null,
+    description: "One Bedroom Condo Downtown/Gulch"
+  };
+  
+  this.state = {
+    ownerID: localStorage.getItem(LoginString.FirebaseDocumentId),
+    name: "Chris Williams",
+    address: "422 2nd Avenue",
+    province: "Atlanta",
+    zipC: "30355",
+    minBid: "250",
+    pic: null,
+    description: "Two Bedroom Condo Buckhead"
+  };
+
+  this.state = {
+    ownerID: localStorage.getItem(LoginString.FirebaseDocumentId),
+    name: "Jenny Johnson",
+    address: "516 3rd Avenue",
+    province: "Dallas",
+    zipC: "76092",
+    minBid: "1000",
+    pic: null,
+    description: "Four Bedroom House with Pool in Southlake"
+  };
+
+  this.state = {
+    ownerID: localStorage.getItem(LoginString.FirebaseDocumentId),
+    name: "Harriet Johnson",
+    address: "222 5th Avenue",
+    province: "Los Angeles",
+    zipC: "",
+    minBid: "",
+    pic: null,
+    description: ""
+  };
+
+  this.state = {
+    ownerID: localStorage.getItem(LoginString.FirebaseDocumentId),
+    name: "Anthony Ribeiro",
+    address: "362 6th Avenue",
+    province: "Chicago",
+    zipC: "60613",
+    minBid: "1200",
+    pic: null,
+    description: "Five Bedroom Party House Across from Wrigley Field"
+  };
+
+  this.state = {
+    ownerID: localStorage.getItem(LoginString.FirebaseDocumentId),
+    name: "Max Scherzer",
+    address: "641 7th Avenue",
+    province: "Washington D.C.",
+    zipC: "",
+    minBid: "",
+    pic: null,
+    description: ""
+  };
+
+  this.state = {
+    ownerID: localStorage.getItem(LoginString.FirebaseDocumentId),
+    name: "Ashley Akin",
+    address: "483 8th Avenue",
+    province: "Memphis",
+    zipC: "",
+    minBid: "",
+    pic: null,
+    description: ""
+  };
+
+  this.state = {
+    ownerID: localStorage.getItem(LoginString.FirebaseDocumentId),
+    name: "Catherine Walker",
+    address: "747 4th Avenue",
+    province: "New York City",
+    zipC: "10023",
+    minBid: "300",
+    pic: null,
+    description: "One Bedroom Apartment on Upper West Side"
+  };
+
+  this.state = {
+    ownerID: localStorage.getItem(LoginString.FirebaseDocumentId),
+    name: "Tom Chong",
+    address: "420 9th Avenue",
+    province: "Denver",
+    zipC: "",
+    minBid: "",
+    pic: null,
+    description: ""
+  };
+
+  this.state = {
+    ownerID: localStorage.getItem(LoginString.FirebaseDocumentId),
+    name: "Ted Williams",
+    address: "406 10th Avenue",
+    province: "Boston",
+    zipC: "",
+    minBid: "",
+    pic: null,
+    description: ""
+  };
