a2c8a763b489d87572510d73373eb5a9314ae9a7
Mason-Shadrick <masonthemason@gmail.com>
2020-09-14 20:45:42 -0500

    card mapping for firebase prop data

--- propupapp/src/components/pages/Results/index.js
+++ propupapp/src/components/pages/Results/index.js
@@ -1,25 +1,104 @@
-import React from "react";
+import React , {Component} from "react";
 import './styles.css';
 import "react-bulma-components/dist/react-bulma-components.min.css";
 import Weather from '../Weather/index';
-import Demo4 from '../Results/Demo4.jpg';
-import Demo5 from '../Results/Demo5.jpg';
-import Demo6 from '../Results/Demo6.jpg';
-import Demo7 from '../Results/Demo7.jpg';
+// import Demo4 from '../Results/Demo4.jpg';
+// import Demo5 from '../Results/Demo5.jpg';
+// import Demo6 from '../Results/Demo6.jpg';
+// import Demo7 from '../Results/Demo7.jpg';
+import { Card } from "@material-ui/core";
+import firebase from "../../../auth/";
 
 
-const Results = () => {
+const db = firebase.firestore();
+
+
+
+class Results extends Component {
+    
+    constructor(props){
+        super(props);
+    
+        this.state = {
+            propArray: []
+        }
+    }
+
+ getAll() {
+    
+     db.collection('properties').get().then((snapshot) => (
+        snapshot.forEach((doc) => (
+            db.collection('users').doc(doc.data().ownerID).get().then((snap) => (
+            this.setState((prevState) => ({
+               propArray:[ ...prevState.propArray, 
+                {
+                name: doc.data().name,
+                address: doc.data().address,
+                description: doc.data().description,
+                minBid: doc.data().minBid,
+                province: doc.data().province,
+                pic: doc.data().pic,
+                zipC: doc.data().zipC,
+                owner: snap.data().name
+               }
+            ]
+            }))
+        ))
+        ))
+        ))
+    }
+    componentDidMount(){
+        this.getAll()
+        console.log(this.state.propArray)
+    }
+         
+    
+
+        render(){
+            let displayPosts = this.state.propArray.map((p) => (
+                <div className="column">
+                <div className="card">
+                    <div className="card-image">
+                        <figure className="image is-4by3">
+                        <img src={p.pic[0]} alt="House1" alt="Placeholder image" />
+                        </figure>
+                    </div>
+                    <div className="card-content">
+                        <div className="media">
+                            <div className="media-left">
+                                <figure className="image is-48x48">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQE9tG_NFfmLde3aA3q3p2yib1KJslRRNlJQg&usqp=CAU" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                            <div className="media-content">
+                                <p className="title is-4" > {p.name} </p>
+                                <p className="subtitle is-6">Listed By: <i>{p.owner}</i></p>
+                            </div>
+                        </div>
+
+                        <div className="content">
+                        <p>{p.description}</p>
+                        <a href="#">#PropUp</a><br></br>
+                        <button className="bid">Make a Bid</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            ))
     return (
         <div className="propresult">
             <div className="column">
                 <h1>Top Matches for your Search:</h1>
             </div>
             <div className="columns is-desktop">
-                <div className="column">
+            <Card>
+                {displayPosts}
+            </Card>
+               {/* <div className="column">
                     <div className="card">
                         <div className="card-image">
                             <figure className="image is-4by3">
-                            <img src={Demo4} alt="House1" alt="Placeholder image" />
+                            <img src={p.pic[0]} alt="House1" alt="Placeholder image" />
                             </figure>
                         </div>
                         <div className="card-content">
@@ -30,21 +109,20 @@ const Results = () => {
                                     </figure>
                                 </div>
                                 <div className="media-content">
-                                    <p className="title is-4">Secluded Getaway</p>
-                                    <p className="subtitle is-6">Listed By: <i>John Smith</i></p>
+                                    <p className="title is-4" value={p.name}></p>
+                                    <p className="subtitle is-6">Listed By: <i>fix this later</i></p>
                                 </div>
                             </div>
 
-                            <div className="content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Phasellus nec iaculis mauris.
+                            <div className="content" value={p.description}>
+                            
                             <a href="#">#PropUp</a><br></br>
                             <button className="bid">Make a Bid</button>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="column">
+                </div> */}
+                {/* <div className="column">
                     <div className="card">
                         <div className="card-image">
                             <figure className="image is-4by3">
@@ -130,11 +208,11 @@ const Results = () => {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */} 
             </div><br></br>        
         <Weather />
     </div>
     )
 };
-
+}
 export default Results;      
